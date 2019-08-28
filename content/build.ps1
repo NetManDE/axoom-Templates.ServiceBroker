44deeb5d33016498cbd45ac75411902d6ba728be
Bastian Eicher <bastian.eicher@axoom.com>
2019-08-28 19:57:20 +0200

    Removed clean from build scripts

--- content/build.ps1
+++ content/build.ps1
@@ -2,8 +2,6 @@
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot\src
 
-dotnet clean MyVendor.ServiceBroker.sln
-
 dotnet msbuild /t:Restore /t:Build /t:Publish /p:PublishDir=./obj/Docker/publish /p:Configuration=Release /p:Version=$Version MyVendor.ServiceBroker.sln
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
